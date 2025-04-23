a7dfb439e9025ecac9d3b06f6b1fd4d5b1913f06
Karthyayeni_ <kavyakrithi0908@gmail.com>
2025-04-23 22:35:25 +0530

    Test

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
